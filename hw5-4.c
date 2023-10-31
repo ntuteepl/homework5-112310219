813232fb91e709e99112c4ae92526e2e5b527226
112310219 <146065173+112310219@users.noreply.github.com>
2023-10-31 14:25:06 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int hr , min ;
+    float hangle , mangle , output;
+    scanf("%d%d" , &hr , &min);
+    hangle = (hr * 30) + (min * 0.5);
+    mangle = (min * 6);
+    if (hangle < mangle){
+        output = mangle - hangle;
+    }
+    else if (hangle > mangle){
+        output = hangle - mangle;
+    }
+    if (output > 180){
+        output = output - 180;
+    }
+    else if (output < 0){
+        output = output + 180;
+    }
+    printf("%0.3f" , output);
+
+}
 
